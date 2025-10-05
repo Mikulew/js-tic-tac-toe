3b885a0f71210349c85ba6c22e3a082e9ddb9707
Mikulew <mikulew@gmail.com>
2025-10-05 15:03:23 +0200

    Add draw and winning game logic

--- consts/index.js
+++ consts/index.js
@@ -2,3 +2,15 @@ export const CROSS_CLASS = "x";
 export const NAUGHT_CLASS = "o";
 export const CROSS_TITLE = "X's turn";
 export const NAUGHT_TITLE = "O's turn";
+export const DRAW_TITLE = "This is a draw!";
+export const WINNER_TITLE = mark => `${mark} is a winner!`;
+export const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
--- utils/game.js
+++ utils/game.js
@@ -1,21 +1,41 @@
-import { CROSS_CLASS, CROSS_TITLE, NAUGHT_CLASS, NAUGHT_TITLE } from "../consts/index.js";
+import {
+  CROSS_CLASS,
+  NAUGHT_CLASS,
+  CROSS_TITLE,
+  NAUGHT_TITLE,
+  DRAW_TITLE,
+  WINNER_TITLE,
+  WINNING_COMBINATIONS,
+} from "../consts/index.js";
 
 const cellElements = document.querySelectorAll("[data-cell]");
 const gameboard = document.querySelector(".gameboard:not(.presentation)");
 const title = document.getElementsByClassName("turn-title")[0];
 let currentMark = CROSS_CLASS;
+const cells = ["", "", "", "", "", "", "", "", ""];
 
 export const initGame = () => {
-  cellElements.forEach(cellElement => {
-    cellElement.addEventListener('click', handleClick, { once: true });
+  cellElements.forEach((cellElement, index) => {
+    cellElement.addEventListener('click', (e) => handleClick(e, index), { once: true });
   });
 };
 
-function handleClick(e) {
+function handleClick(e, index) {
   const cell = e.target;
-  changeClass(cell, currentMark);
-  changeTitle(getOppositeMark(currentMark));
-  changeMark(getOppositeMark(currentMark));
+    const mark = currentMark;
+    changeValue(index, mark);
+    changeTitle(mark);
+    changeClass(cell, mark);
+    if (checkWin(mark)) {
+      return endGame(mark);
+    } else if (isDraw()) {
+      return endGame(mark);
+    }
+    changeMark(getOppositeMark(mark));
+}
+
+function changeValue(index, value) {
+  cells[index] = value;
 }
 
 function changeMark(newMark) {
@@ -23,7 +43,12 @@ function changeMark(newMark) {
 }
 
 function changeTitle(mark) {
-  title.textContent = mark === CROSS_CLASS ? CROSS_TITLE : NAUGHT_TITLE;
+  if (checkWin(mark)) {
+    return title.textContent = WINNER_TITLE(mark);
+  } else if (isDraw()) {
+    return title.textContent = DRAW_TITLE;
+  }
+  return title.textContent = getOppositeMark(mark) === CROSS_CLASS ? CROSS_TITLE : NAUGHT_TITLE;
 }
 
 function getOppositeMark(mark) {
@@ -34,4 +59,19 @@ function changeClass(cell, mark) {
   cell.classList.add(mark);
   gameboard.classList.remove(mark);
   gameboard.classList.add(getOppositeMark(mark));
+}
+
+function checkWin(mark) {
+  return WINNING_COMBINATIONS.some(
+    combination => combination.every(index => cells[index] === mark)
+  );
+}
+
+function isDraw() {
+  return cells.every(cell => cell.length !== 0);
+}
+
+function endGame(mark) {
+  gameboard.classList.remove(getOppositeMark(mark));
+    gameboard.classList.add('presentation');
 }
