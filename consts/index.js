c909a7612cd5117c2862e8c45dfc57b5f62d166b
Mikulew <mikulew@gmail.com>
2025-10-15 19:57:47 +0200

    Refactor consts and getters file

--- consts/index.js
+++ consts/index.js
@@ -1,27 +1,22 @@
 export const CROSS_CLASS = "x";
 export const NAUGHT_CLASS = "o";
+
 export const MARK = {
   CROSS: "X",
   NAUGHT: "O",
 };
+
 export const CLASS_NAME = {
   [MARK.CROSS]: CROSS_CLASS,
   [MARK.NAUGHT]: NAUGHT_CLASS,
 };
-export const CROSS_TITLE = "X's turn";
-export const NAUGHT_TITLE = "O's turn";
-export const DRAW_TITLE = "This is a draw!";
-export const WINNER_TITLE = mark => `${mark} is a winner!`;
+
 export const WINNING_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 3, 6],
-  [0, 4, 8],
-  [2, 4, 6],
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+  [0, 4, 8], [2, 4, 6],            // Diagonals
 ];
+
 export const GAME_STATUS = {
   CROSS_TURN: "CROSS_TURN",
   NAUGHT_TURN: "NAUGHT_TURN",
@@ -29,12 +24,22 @@ export const GAME_STATUS = {
   NAUGHT_WINS: "NAUGHT_WINS",
   DRAW: "DRAW",
 };
-export const GAME_TITLE = {
-  [GAME_STATUS.PLAY]: "Play game!",
-  [GAME_STATUS.CROSS_TURN]: `${WINNER_TITLE(MARK.CROSS)}`,
-  [GAME_STATUS.NAUGHT_TURN]: `${WINNER_TITLE(MARK.NAUGHT)}`,
-  [GAME_STATUS.DRAW]: " This is a draw!",
+
+export const TITLES = {
+  [GAME_STATUS.CROSS_TURN]: "X's turn",
+  [GAME_STATUS.NAUGHT_TURN]: "O's turn",
+  [GAME_STATUS.DRAW]: "This is a draw!",
+  WINNER: mark => `${mark} is a winner!`,
 };
+
+export const GAME_TITLE = {
+  [GAME_STATUS.CROSS_TURN]: TITLES.CROSS_TURN,
+  [GAME_STATUS.NAUGHT_TURN]: TITLES.NAUGHT_TURN,
+  [GAME_STATUS.CROSS_WINS]: TITLES.WINNER(MARK.CROSS),
+  [GAME_STATUS.NAUGHT_WINS]: TITLES.WINNER(MARK.NAUGHT),
+  [GAME_STATUS.DRAW]: TITLES.DRAW,
+};
+
 export const HTML_ELEMENTS = {
   MENU_VIEW: "MENU_VIEW",
   GAMEBOARD_VIEW: "GAMEBOARD_VIEW",
--- utils/getters.js
+++ utils/getters.js
@@ -1,40 +1,26 @@
 import {
   CLASS_NAME,
-  CROSS_TITLE,
-  NAUGHT_TITLE,
-  DRAW_TITLE,
-  WINNER_TITLE,
+  GAME_TITLE,
   GAME_STATUS,
   MARK,
   HTML_ELEMENTS,
 } from "../consts/index.js";
 import { isArrayLike } from "./utilities.js";
 
-const menuSection = document.getElementById("menu");
-const gameboardSection = document.getElementById("gameboard");
-const secondPlayerButton = document.getElementById("secondPlayer");
-const computerPlayerButton = document.getElementById("computerPlayer");
-const abortGameButton = document.getElementById("abortGame");
-const restartGameButton = document.getElementById("restartGame");
-let cellElements = document.querySelectorAll("[data-cell]");
-const gameboard = document.getElementById("playground");
-let title = document.getElementsByClassName("turn-title")[0];
+const elements = {
+  [HTML_ELEMENTS.MENU_VIEW]: document.getElementById("menu"),
+  [HTML_ELEMENTS.GAMEBOARD_VIEW]: document.getElementById("gameboard"),
+  [HTML_ELEMENTS.SECOND_PLAYER_BUTTON]: document.getElementById("secondPlayer"),
+  [HTML_ELEMENTS.COMPUTER_PLAYER_BUTTON]: document.getElementById("computerPlayer"),
+  [HTML_ELEMENTS.ABORT_GAME_BUTTON]: document.getElementById("abortGame"),
+  [HTML_ELEMENTS.RESTART_GAME_BUTTON]: document.getElementById("restartGame"),
+  [HTML_ELEMENTS.PLAYGROUND]: document.getElementById("playground"),
+  [HTML_ELEMENTS.CELLS]: document.querySelectorAll("[data-cell]"),
+  [HTML_ELEMENTS.TITLE]: document.getElementsByClassName("turn-title")[0],
+};
 
 export function getTitle(status) {
-  switch(status) {
-    case GAME_STATUS.CROSS_TURN:
-      return CROSS_TITLE;
-    case GAME_STATUS.NAUGHT_TURN:
-      return NAUGHT_TITLE;
-    case GAME_STATUS.CROSS_WINS:
-      return WINNER_TITLE(MARK.CROSS);
-    case GAME_STATUS.NAUGHT_WINS:
-      return WINNER_TITLE(MARK.NAUGHT);
-    case GAME_STATUS.DRAW:
-      return DRAW_TITLE;
-    default:
-      return CROSS_TITLE;
-  }
+  return GAME_TITLE[status] ?? GAME_TITLE[GAME_STATUS.CROSS_TURN];
 }
 
 export function getOppositeMark(mark) {
@@ -42,37 +28,18 @@ export function getOppositeMark(mark) {
 }
 
 export function getHTMLElement(element) {
-  switch(element) {
-    case HTML_ELEMENTS.MENU_VIEW:
-      return menuSection;
-    case HTML_ELEMENTS.GAMEBOARD_VIEW:
-      return gameboardSection;
-    case HTML_ELEMENTS.SECOND_PLAYER_BUTTON:
-      return secondPlayerButton;
-    case HTML_ELEMENTS.COMPUTER_PLAYER_BUTTON:
-      return computerPlayerButton;
-    case HTML_ELEMENTS.ABORT_GAME_BUTTON:
-      return abortGameButton;
-    case HTML_ELEMENTS.RESTART_GAME_BUTTON:
-      return restartGameButton;
-    case HTML_ELEMENTS.PLAYGROUND:
-      return gameboard;
-    case HTML_ELEMENTS.CELLS:
-      return cellElements;
-    case HTML_ELEMENTS.TITLE:
-      return title;
-  }
+  return elements[element];
 }
 
 export function refreshHTMLElement(element) {
   switch(element) {
     case HTML_ELEMENTS.CELLS: {
-      cellElements.forEach(cellElement => {
+      elements[element].forEach(cellElement => {
         const newCell = cellElement.cloneNode(true);
         cellElement.parentNode.replaceChild(newCell, cellElement);
       });
-      cellElements = document.querySelectorAll("[data-cell]");
-      return cellElements;
+      elements[element] = document.querySelectorAll("[data-cell]");
+      return elements[element];
     }
     default:
       return new Error("Unsupported HTML element");
@@ -81,13 +48,13 @@ export function refreshHTMLElement(element) {
 
 export function getEmptyMark(element) {
   if (isArrayLike(element)) {
-    const newElement = [...element];
-    newElement.forEach(e => {
+    [...element].forEach(e => {
       e.classList.remove(CLASS_NAME[MARK.CROSS]);
       e.classList.remove(CLASS_NAME[MARK.NAUGHT]);
     });
-    return newElement;
+    return element;
   }
   element.classList.remove(CLASS_NAME[MARK.CROSS]);
   element.classList.remove(CLASS_NAME[MARK.NAUGHT]);
+  return element;
 }
